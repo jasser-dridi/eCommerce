abfb6c3c5540d71c3d2348fa82f9f57104a9bc66
Jasser Dridi <Jasser.Dridi@keyrus.com>
2022-10-31 17:06:54 +0100

    Adds mailing functionality to user service

--- user-service/src/main/java/com/keyrus/consumer/MailConsumer.java
+++ user-service/src/main/java/com/keyrus/consumer/MailConsumer.java
@@ -5,15 +5,17 @@ import com.keyrus.Service.UserService;
 import com.keyrus.modeldto.EmailDto;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-
+@ApplicationScoped
 public class MailConsumer {
 
     @Inject
     EmailService emailService;
-    @Incoming("mail")
+    @Incoming("mail-in")
     public void sendEmail(EmailDto emailDto)
     {
+        System.out.println("inside sendEmail");
         emailService.sendMail(emailDto.email, emailDto.message, "NOTIFICATION");
     }
 }
